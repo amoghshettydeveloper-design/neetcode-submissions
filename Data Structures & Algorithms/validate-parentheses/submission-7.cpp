0a6e5b9b12115afaadb3a192eddb97093f517887
amoghshettydeveloper-design <amoghshettydeveloper@gmail.com>
2026-05-17 13:01:20 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isOpenPara(char c){
+        return c == '{' || c == '[' || c == '(';
+    }
+
+    bool isClosePara(char c){
+        return c == '}' || c == ']' || c == ')';
+    }
+
+    char getOpenPara(char c){
+        switch(c){
+            case ')' :  c='(';
+                        break;
+            case ']' :  c='[';
+                        break;
+            case '}' :  c='{';
+                        break;
+
+        }
+        return c;
+    }
+
+    bool isValid(string s) {
+        int i=0;
+        stack<char> paraStack;
+        while(i<s.size()){
+            
+            while(isOpenPara(s[i])){
+                paraStack.push(s[i++]);
+            }
+            
+            if(isClosePara(s[i])){
+                if(paraStack.empty()){
+                    return false;
+                }
+                else{
+                    
+                    if( paraStack.top() != getOpenPara(s[i]))
+                        return false;
+                    else
+                        paraStack.pop();
+                }
+            }
+            i++;
+        }
+        return paraStack.empty();
+    }
+};
