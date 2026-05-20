f229b10bd3d197d7b735e5083fade3c17af3a130
amoghshettydeveloper-design <amoghshettydeveloper@gmail.com>
2026-05-20 11:09:45 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.cpp
@@ -0,0 +1,24 @@
+// Approach: Bitmask (O(1) time & space) - each int tracks seen digits as bits
+class Solution {
+   public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<int> rows(9, 0);
+        vector<int> cols(9, 0);
+        vector<int> boxes(9, 0);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char cur = board[i][j];
+                if (cur == '.') continue;
+                int c = cur - '0';
+                int bit = 1 << c;
+                int box = (j / 3) + (i / 3) * 3;
+                if (rows[i] & bit || cols[j] & bit || boxes[box] & bit) return false;
+                rows[i] |= bit;
+                cols[j] |= bit;
+                boxes[box] |= bit;
+            }
+        }
+        return true;
+    }
+};
