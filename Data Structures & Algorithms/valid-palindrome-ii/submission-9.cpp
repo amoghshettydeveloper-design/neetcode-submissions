9c7c84124428fbbe6a045dd0df73196a47837f30
amoghshettydeveloper-design <amoghshettydeveloper@gmail.com>
2026-05-23 10:56:25 +0530

    Add: valid-palindrome-ii - submission-9

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-9.cpp
@@ -0,0 +1,19 @@
+// Optimized by Claude
+class Solution {
+    bool isPalin(string& s, int i, int j) {
+        while (i < j) {
+            if (s[i++] != s[j--]) return false;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (s[i] != s[j])
+                return isPalin(s, i+1, j) || isPalin(s, i, j-1);
+            i++; j--;
+        }
+        return true;
+    }
+};
