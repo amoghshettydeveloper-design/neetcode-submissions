0f6b4c38d8e2ba09e13dde8e32963cc436b39ee5
amoghshettydeveloper-design <amoghshettydeveloper@gmail.com>
2026-05-23 10:51:52 +0530

    Add: valid-palindrome-ii - submission-8

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-8.cpp
@@ -0,0 +1,39 @@
+class Solution {
+   public:
+    bool validPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        bool deleted = false;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                if (i == j) {
+                    return false;
+                }
+                deleted = true;
+                break;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        if (!deleted) return true;
+        int ogi = i;
+        int ogj = j;
+        i++;
+        bool flag = true;
+        while (i < j) {
+            if (s[i++] != s[j--]) {
+                flag = false;
+                break;
+            }
+        }
+        i = ogi;
+        j = ogj - 1;
+        if (!flag) {
+            while (i < j) {
+                if (s[i++] != s[j--]) return false;
+            }
+            flag = true;
+        }
+        return flag;
+    }
+};
