5999b0fef818a069052b7b64d37c93594e30c96e
amoghshettydeveloper-design <amoghshettydeveloper@gmail.com>
2026-05-26 23:27:35 +0530

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.cpp
@@ -0,0 +1,19 @@
+// Solution which protects on overflow incase of large int values
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int i = 0, j = nums.size() - 1;
+
+        while (i <= j) {
+            int mid = i + (j-i) / 2; // for potential overflow issues
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                j = mid - 1;
+            else if (nums[mid] < target)
+                i = mid + 1;
+        }
+
+        return -1;
+    }
+};
