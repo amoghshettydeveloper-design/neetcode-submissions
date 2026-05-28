4704aa904176e188575f7f6162a51fac105aa38b
amoghshettydeveloper-design <amoghshettydeveloper@gmail.com>
2026-05-29 01:30:22 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.cpp
@@ -0,0 +1,32 @@
+// Best Solution
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0, tens = 0;
+
+        for(int n:bills){
+            if(n == 5)
+                fives++;
+            else if(n == 10){
+                if(fives){
+                    fives--;
+                    tens++;
+                }
+                else
+                    return false;
+            }
+            else if(n == 20){
+                if(fives && tens){
+                    fives--;
+                    tens--;
+                }
+                else if(fives>=3){
+                    fives -= 3;
+                }
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+};
