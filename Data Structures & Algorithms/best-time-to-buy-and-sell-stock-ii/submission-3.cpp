fe2d987fcce431f8b9b33caa8cc80aa35b00d727
amoghshettydeveloper-design <amoghshettydeveloper@gmail.com>
2026-06-03 10:06:29 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-3

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0], profit = 0;
+
+        for(int i = 1; i<prices.size(); i++){
+            if(prices[i]<min)
+                min = prices[i];
+            else if(prices[i]>min){
+                profit += (prices[i] - min);
+                min = prices[i];
+            }
+        }
+
+        return profit;
+    }
+};
